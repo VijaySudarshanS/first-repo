6d8fc955bae9560edef1878717da99d0e407e82c
VijaySudarshanS <vijaypococ55@gmail.com>
2025-07-21 15:19:18 +0530

    Revert "notionn of buttion"
    This reverts commit 466e3691ba94dcb89f463e6508d8fdf56f1ddd0d.

--- bulb.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('glowBtn').addEventListener('click', function () {
-  const content = document.getElementById('hiddenContent');
-  content.style.display = content.style.display === 'none' ? 'block' : 'none';
-});
