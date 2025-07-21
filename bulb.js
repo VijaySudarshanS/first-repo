466e3691ba94dcb89f463e6508d8fdf56f1ddd0d
VijaySudarshanS <vijaypococ55@gmail.com>
2025-07-21 14:58:50 +0530

    notionn of buttion
    basic bulb sunction

---
+++ bulb.js
@@ -0,0 +1,4 @@
+document.getElementById('glowBtn').addEventListener('click', function () {
+  const content = document.getElementById('hiddenContent');
+  content.style.display = content.style.display === 'none' ? 'block' : 'none';
+});
